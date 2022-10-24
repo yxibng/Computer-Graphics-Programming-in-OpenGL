5381e590c3bdfa87218811c4a034c665c958a3ad
yxibng <yxibng@gmail.com>
2022-10-24 16:47:15 +0800

    feat: cmake importing and exporting guide

--- Programs/Chapter_02_opengl/Prog2_1_background/main.cpp
+++ Programs/Chapter_02_opengl/Prog2_1_background/main.cpp
@@ -1,6 +1,11 @@
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+
+
 
 using namespace std;
 
@@ -14,9 +19,17 @@ void display(GLFWwindow* window, double currentTime) {
 int main(void) {
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
 	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
 	glfwMakeContextCurrent(window);
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
 	glfwSwapInterval(1);
 
