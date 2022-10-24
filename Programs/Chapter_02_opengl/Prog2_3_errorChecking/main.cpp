f8704aa91b2124ace44de4d79e7026300e6946e6
yxibng <yxibng@gmail.com>
2022-10-24 17:26:51 +0800

    feat: Prog2_3_errorChecking

--- Programs/Chapter_02_opengl/Prog2_3_errorChecking/main.cpp
+++ Programs/Chapter_02_opengl/Prog2_3_errorChecking/main.cpp
@@ -1,5 +1,5 @@
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 
@@ -51,12 +51,12 @@ GLuint createShaderProgram() {
 	GLint linked;
 
 	const char *vshaderSource =
-		"#version 430    \n"
+		"#version 410    \n"
 		"void main(void) \n"
 		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
 
 	const char *fshaderSource =
-		"#version 430    \n"
+		"#version 410    \n"
 		"out vec4 color; \n"
 		"void main(void) \n"
 		"{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
@@ -123,7 +123,9 @@ void display(GLFWwindow* window, double currentTime) {
 int main(void) {
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 3", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
